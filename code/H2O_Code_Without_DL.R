367228c52f5c6372cd278fd841b9a77102be6534
José Francisco Vélez Serrano <jose.velez@urjc.es>
2020-05-23 10:45:50 +0200

    Update H2O_Code_Without_DL.R

--- code/H2O_Code_Without_DL.R
+++ code/H2O_Code_Without_DL.R
@@ -34,3 +34,48 @@ testData$ID <- NULL
 
 # H2O environment ---------------------------------------------------------
 library(bit64,lib="./MyRlibs")
+library(h2o,lib="./MyRlibs")
+
+h2o.init(nthreads = -2)
+
+# Upload data to H2O
+
+trainData.h2o <- as.h2o(trainData)
+validationData.h2o <- as.h2o(validationData)
+testData.h2o <- as.h2o(testData)
+
+# Variable definition to H2O
+# NOTE: Set the corresponding target name between "" after the %in%:
+
+inputs <- trainData[, ! names(trainData) %in% "TARGET", drop = F]
+predictors <- names(inputs)
+
+
+# AutoML ----------------------------------------------------------------
+
+mySeed <- 12345
+set.seed(mySeed)
+maxNumModels <- 10
+
+# NOTE: Set the corresponding target name in the y variable:
+
+aml <- h2o.automl(x = predictors,
+                  y = "TARGET", 
+                  training_frame = trainData.h2o,
+                  validation_frame = validationData.h2o,
+                  leaderboard_frame = testData.h2o,
+                  nfolds = 0,
+                  max_runtime_secs = 10800, 
+                  max_models = maxNumModels,
+                  stopping_metric = "MAE",
+                  stopping_rounds = 100,
+                  sort_metric = "MAE",
+                  exclude_algos = c("DeepLearning","StackedEnsemble"),
+                  seed = mySeed)
+
+errorsTable <- as.data.frame(aml@leaderboard)
+
+# exitPath <- "../errors"
+
+write.csv(errorsTable, file = paste0("Errors_",datasetName,"_",maxNumModels,"models_MAE.csv"))
+print(errorsTable)
