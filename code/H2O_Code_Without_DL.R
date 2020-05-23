3553d4923df61f5f65c2a4aa128a4d2bb1311d8c
José Francisco Vélez Serrano <jose.velez@urjc.es>
2020-05-23 10:43:42 +0200

    Update H2O_Code_Without_DL.R

--- code/H2O_Code_Without_DL.R
+++ code/H2O_Code_Without_DL.R
@@ -20,3 +20,17 @@ trainData <- read.csv(paste0(trainDataName,".csv"), stringsAsFactors = FALSE)
 validationData <- read.csv(paste0(validationDataName,".csv"), stringsAsFactors = FALSE)
 testData <- read.csv(paste0(testDataName,".csv"), stringsAsFactors = FALSE)
 
+
+# Data preparation --------------------------------------------------------
+
+trainData_ID <- trainData$ID
+validationData_ID <- validationData$ID
+testData_ID <- testData$ID
+
+trainData$ID <- NULL
+validationData$ID <- NULL
+testData$ID <- NULL
+
+
+# H2O environment ---------------------------------------------------------
+library(bit64,lib="./MyRlibs")
