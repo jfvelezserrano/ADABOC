06c782f743937fe8807ce6ee3bc8a78fbacdabda
danielvelezserrano <50073425+danielvelezserrano@users.noreply.github.com>
2020-06-08 22:27:53 +0200

    Update H2O_Code_Only_DL.R

--- code/H2O_Code_Only_DL.R
+++ code/H2O_Code_Only_DL.R
@@ -4,7 +4,7 @@ library(h2o,lib="~/MyRlibs")
 # Specify dataset ---------------------------------------------------------
 
 datasetDirName <- "datasets"
-datasetName <- "Concrete_sinAge"
+datasetName <- "electrical_grid"
 
 # Load data ---------------------------------------------------------------
 
