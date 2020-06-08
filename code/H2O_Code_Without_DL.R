0a9314db81b87154c7efcd54e7e6ee0d618686a8
danielvelezserrano <50073425+danielvelezserrano@users.noreply.github.com>
2020-06-08 19:02:45 +0200

    Update H2O_Code_Without_DL.R

--- code/H2O_Code_Without_DL.R
+++ code/H2O_Code_Without_DL.R
@@ -4,7 +4,7 @@
 # Dataset -----------------------------------------------------------------
 
 datasetDirName <- "datasets"
-datasetName <- "Parkinsons1"
+datasetName <- "Airfoil"
 
 # Load data ---------------------------------------------------------------
 
