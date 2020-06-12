3312c003a4f21ed5984d1d93537cc6f1ac8d191c
danielvelezserrano <50073425+danielvelezserrano@users.noreply.github.com>
2020-06-12 19:42:51 +0200

    Update H2O_Code_Only_DL.R

--- code/H2O_Code_Only_DL.R
+++ code/H2O_Code_Only_DL.R
@@ -4,7 +4,7 @@ library(h2o,lib="~/MyRlibs")
 # Specify dataset ---------------------------------------------------------
 
 datasetDirName <- "datasets"
-datasetName <- "Bias2"
+datasetName <- "Parkinsons2"
 
 # Load data ---------------------------------------------------------------
 
