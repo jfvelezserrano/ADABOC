be9f0fbf21e4f0a8db66aeb3cb6645be90d98154
danielvelezserrano <50073425+danielvelezserrano@users.noreply.github.com>
2021-02-03 01:48:41 +0100

    Update nestedCopulasModelExample.R

--- code/nestedCopulasModelExample.R
+++ code/nestedCopulasModelExample.R
@@ -1,7 +1,7 @@
 options(width=180)
 
 # Load data --------------------------------------------------------------
-datasetName    <- "Parkinsons2"
+datasetName    <- "electrical_grid"
 datasetDirName <- "datasets"
 
 trainDataName      <- paste0(datasetDirName, "/", datasetName,"_train",      ".csv")
