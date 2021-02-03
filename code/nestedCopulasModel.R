4769ca95ede6c18f5076dd68e8d54250c7f01b5c
danielvelezserrano <50073425+danielvelezserrano@users.noreply.github.com>
2021-02-03 12:32:51 +0100

    Update nestedCopulasModel.R

--- code/nestedCopulasModel.R
+++ code/nestedCopulasModel.R
@@ -213,14 +213,14 @@ nestedCopulasModel    <- function(trainingDataset = NULL,
   }
   
   ## we store all the non unary variables in a  table 
-  variables_table <- mapply(c, check_unary_variables, valid_data)
+  variables_table <- as.data.frame(mapply(c, check_unary_variables, valid_data))
   print("COMPROBACION 1")
   print(head(variables_table))
-  variables_table <- mapply(c, variables_table, test_data)
+  variables_table <- as.data.frame(mapply(c, variables_table, test_data))
   print("COMPROBACION 2")
   print(head(variables_table))
   
-  variables_table <- variables_table[names(variables_table) %in% non_unary_variables[non_unary_variables!="Target"]]
+  variables_table <- variables_table[names(variables_table) %in% non_unary_variables[non_unary_variables!="Target"],]
   print("COMPROBACION 3")
   print(head(variables_table))
   train_variables <- train_variables[names(train_variables) %in% non_unary_variables[non_unary_variables!="Target"]]
