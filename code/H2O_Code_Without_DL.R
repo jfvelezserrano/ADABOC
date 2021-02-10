4112730ef699bff3ca93b0b56379309e93c2fbbe
danielvelezserrano <50073425+danielvelezserrano@users.noreply.github.com>
2021-02-10 10:58:28 +0100

    Update H2O_Code_Without_DL.R

--- code/H2O_Code_Without_DL.R
+++ code/H2O_Code_Without_DL.R
@@ -4,7 +4,7 @@
 # Dataset -----------------------------------------------------------------
 
 datasetDirName <- "datasets"
-datasetName <- "communities_unnormalized"
+datasetName <- "Communities"
 
 # Load data ---------------------------------------------------------------
 
