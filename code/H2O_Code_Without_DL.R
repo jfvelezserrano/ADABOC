837caa06bc48fc67d8f552efdaf246ee0b8f9ce1
danielvelezserrano <50073425+danielvelezserrano@users.noreply.github.com>
2021-02-15 22:52:28 +0100

    Update H2O_Code_Without_DL.R

--- code/H2O_Code_Without_DL.R
+++ code/H2O_Code_Without_DL.R
@@ -4,7 +4,7 @@
 # Dataset -----------------------------------------------------------------
 
 datasetDirName <- "datasets"
-datasetName <- "kdd"
+datasetName <- "bias_correction_ucl_tmed"
 
 # Load data ---------------------------------------------------------------
 
